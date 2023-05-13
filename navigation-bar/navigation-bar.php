c62acb9dc52bcfff524d6487759c8d46b22ffcae
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-05-13 22:27:19 +0300

    Update navigation-bar.php

--- navigation-bar/navigation-bar.php
+++ navigation-bar/navigation-bar.php
@@ -3,7 +3,7 @@
     <ul id="NavigationBar">
         <li><a class="active" href="../home/index.php">Home</a></li>
         <li><a href="../membrii/membrii.php">Membrii</a></li>
-        <li><a href="../program/index.php">Program</a></li>
-        <li><a href="../repertoriu/index.php">Repertoriu</a></li>
+        <li><a href="../program/program.php">Program</a></li>
+        <li><a href="../repertoriu/repertoriu.php">Repertoriu</a></li>
         <li><a href="../contact/contact.php">Contact</a></li>
     </ul>
